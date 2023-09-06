d3f31eb1ba8f5b96424028c2583763fcea4aa676
Joe Willis <58248380+codecojoejoe@users.noreply.github.com>
2023-09-05 23:34:51 -0500

    Update tfe_workspaces.tf

--- tfe_workspaces.tf
+++ tfe_workspaces.tf
@@ -8,16 +8,3 @@ resource "tfe_workspace" "okta_terraform" {
   allow_destroy_plan = false
   terraform_version  = "1.0.8"
 }
-
-# ------------------------------------------------------------------------------------------
-# Okta sandbox
-
-/*
-resource "tfe_workspace" "okta_sandbox_terraform" {
-  name               = "okta-sandbox-terraform"
-  organization       = var.org_name
-  description        = "Workspace to build the Included Health Okta sandbox org via infrastructure-as-code"
-  allow_destroy_plan = false
-  terraform_version  = "1.0.8"
-}
-*/
